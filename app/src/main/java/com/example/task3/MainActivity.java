552c36d6285e30547c731d8ceb29c0ad6d32071c
DiasAkbar <13516144@std.stei.itb.ac.id>
2019-01-27 21:25:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/task3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.task3;
+
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mShareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShareTextEditText = (EditText) findViewById(R.id.share_edittext);
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
+    }
+}
